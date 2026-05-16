9c1974c7a97683ff77a49a4d0ae23a83a37e3b02
Melvin Vermast <143947718+Smulvin@users.noreply.github.com>
2026-05-16 09:03:10 +0200

    code voor switch2 teksten, geen styling

---
+++ Scripts/switch2-screen.js
@@ -0,0 +1,92 @@
+const nerdButtons = document.querySelectorAll(".nerd-btn");
+const nerdScreens = document.querySelectorAll(".nerd-screen");
+
+const nerdMenu = document.querySelector(".nerd-menu");
+
+const leftBtn = document.getElementById("btn-left");
+const rightBtn = document.getElementById("btn-right");
+const aBtn = document.getElementById("btn-a");
+const bBtn = document.getElementById("btn-b");
+
+let currentNerd = 0;
+let viewingScreen = false;
+
+
+
+function updateSelection() {
+
+    nerdButtons.forEach(button => {
+        button.classList.remove("active");
+    });
+
+    nerdButtons[currentNerd].classList.add("active");
+}
+
+
+
+leftBtn.addEventListener("click", () => {
+
+    if (viewingScreen) return;
+
+    currentNerd--;
+
+    if (currentNerd < 0) {
+        currentNerd = nerdButtons.length - 1;
+    }
+
+    updateSelection();
+});
+
+
+
+rightBtn.addEventListener("click", () => {
+
+    if (viewingScreen) return;
+
+    currentNerd++;
+
+    if (currentNerd >= nerdButtons.length) {
+        currentNerd = 0;
+    }
+
+    updateSelection();
+});
+
+
+
+aBtn.addEventListener("click", () => {
+
+    if (viewingScreen) return;
+
+    viewingScreen = true;
+
+    const selectedButton = nerdButtons[currentNerd];
+    const target = selectedButton.dataset.nerd;
+
+    nerdMenu.classList.add("hidden");
+
+    nerdScreens.forEach(screen => {
+        screen.classList.remove("active-screen");
+    });
+
+    document
+        .querySelector(`[data-screen="${target}"]`)
+        .classList.add("active-screen");
+});
+
+
+
+bBtn.addEventListener("click", () => {
+
+    viewingScreen = false;
+
+    nerdScreens.forEach(screen => {
+        screen.classList.remove("active-screen");
+    });
+
+    nerdMenu.classList.remove("hidden");
+});
+
+
+
+updateSelection();
