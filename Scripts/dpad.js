8e1105603fe5b197a0371f6679935a5710231feb
Melvin Vermast <143947718+Smulvin@users.noreply.github.com>
2026-06-12 20:06:23 +0200

    real dpad fix

--- Scripts/dpad.js
+++ Scripts/dpad.js
@@ -1,40 +1,44 @@
-const dpad = document.querySelector(".dpad")
+const dpad = document.querySelector(".dpad");
+
 const buttons = {
   BtnTop: document.querySelector("#dpad-up"),
   BtnLeft: document.querySelector("#dpad-left"),
   BtnRight: document.querySelector("#dpad-right"),
   BtnBottom: document.querySelector("#dpad-down"),
+};
+
+function removePressedClasses() {
+  dpad.classList.remove(
+    "top-pressed",
+    "left-pressed",
+    "right-pressed",
+    "bottom-pressed"
+  );
 }
 
-Object.keys(buttons).forEach(id => {
-  const btn = buttons[id]
+Object.entries(buttons).forEach(([id, btn]) => {
 
-  const removePressedClasses = () => {
-    dpad.classList.remove("top-pressed", "left-pressed", "right-pressed", "bottom-pressed")
-  }
+  if (!btn) return;
 
-  btn.addEventListener("mousedown", () => {
-    requestAnimationFrame(() => {
-      removePressedClasses();
+  btn.addEventListener("click", () => {
 
-      switch (id) {
-        case "BtnTop":
-          dpad.classList.add("top-pressed");
-          break;
-        case "BtnLeft":
-          dpad.classList.add("left-pressed");
-          break;
-        case "BtnRight":
-          dpad.classList.add("right-pressed");
-          break;
-        case "BtnBottom":
-          dpad.classList.add("bottom-pressed");
-          break;
-      }
-    })
-  })
+    removePressedClasses();
 
-  // Remove pressed classes when button is released
-  btn.addEventListener("mouseup", removePressedClasses)
-  btn.addEventListener("mouseleave", removePressedClasses)
-})
+    switch (id) {
+      case "BtnTop":
+        dpad.classList.add("top-pressed");
+        break;
+      case "BtnLeft":
+        dpad.classList.add("left-pressed");
+        break;
+      case "BtnRight":
+        dpad.classList.add("right-pressed");
+        break;
+      case "BtnBottom":
+        dpad.classList.add("bottom-pressed");
+        break;
+    }
+
+    setTimeout(removePressedClasses, 120);
+  });
+});
